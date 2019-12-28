7caaa67914548454d06d42d0bce50f044577298c
Murphy4444 <elias@swisstux.net>
2019-12-28 23:18:43 +0100

    Trying stuff

--- Music.py
+++ Music.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env pybricks-micropython
-""" 
-=== IMPORTS ===
-"""
+
 from pybricks import ev3brick as brick
-from pybricks.ev3devices import (
-    Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import (
-    Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
-from pybricks.tools import print, wait, StopWatch
-from pybricks.robotics import DriveBase
 import time
 
-
-# Signalise start of Program
-brick.sound.beep()
-time.sleep(.1)
-""" 
-=== CODE ===
-"""
-
+SongNotes = [246.94,220.00,207.65,220.00,261.63,261.63,0,293.66,261.63,246.94,261.63,329.63,0,0,349.23,329.63,311.13,329.63,493.88,440.00,415.30,440.00,493.88,440.00,415.30,440.00,523.25]
+for i in SongNotes:
+    if(i!=0):
+        brick.sound.beep((i*2),150,1)
+    else:
+        time.sleep(.2)
 
 # Signalise end of Program
-brick.sound.beep()
-time.sleep(.1)
-brick.sound.beep()
-time.sleep(.1)
-brick.sound.beep()
+# time.sleep(3)
+# brick.sound.beep()
+# time.sleep(.1)
+# brick.sound.beep()
+# time.sleep(.1)
+# brick.sound.beep()
--- main.py
+++ main.py
@@ -34,21 +34,36 @@ ColorSensor = ColorSensor(Port.S3)
 
 # endregion Definitions
 
-robot = DriveBase(LeftFootMotor, RightFootMotor, 42, 160)
-drive = True
-while drive:
-    robot.drive(200, 0)
-    while ColorSensor.color() == Color.BLACK:
-        robot.drive_time(-100, 0, 2000)
-        robot.drive_time(0, 60, 2000)
-    wait(10)
-    if (TouchSensor.pressed()):
-        drive = False
-        robot.drive(0, 0)
-    
+# robot = DriveBase(LeftFootMotor, RightFootMotor, 42, 160)
+# drive = True
+# while drive:
+#     robot.drive(200, 0)
+#     if ColorSensor.color() == Color.BLACK:
+#         robot.drive_time(-100, 0, 2000)
+#         robot.drive_time(0, 60, 2000)
+#     wait(10)
+#     if (TouchSensor.pressed()):
+#         drive = False
+#         robot.drive(0, 0)
 
+# while ColorSensor.color() != Color.BLACK:
+#     if not (TouchSensor.pressed()):
+#         brick.display.image(ImageFile.WINKING)
+#         brick.sound.file(SoundFile.BLUE)
+#         brick.sound.file(SoundFile.GREEN)
+# time.sleep(1)
 
-RightFootMotor.run_target(500, 340)
+while not (TouchSensor.pressed()):
+    if (Button.CENTER in brick.buttons()):
+        brick.sound.file(SoundFile.MOTOR_START)
+    if (Button.LEFT in brick.buttons()):
+        brick.sound.file(SoundFile.LEFT)
+    if (Button.UP in brick.buttons()):
+        brick.sound.file(SoundFile.UP)
+    if (Button.RIGHT in brick.buttons()):
+        brick.sound.file(SoundFile.RIGHT)
+    if (Button.DOWN in brick.buttons()):
+        brick.sound.file(SoundFile.DOWN)
 
 # endregion CODE
 
